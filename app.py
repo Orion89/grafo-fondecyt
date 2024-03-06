1931d8ab82ab7fcb42b8906f8153f0a0729d9813
Leo Molina <leonardo.molina.v@gmail.com>
2024-03-06 14:48:37 -0300

    added graph filter functionality

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 from ast import literal_eval
 import json
 
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 from dashvis import DashNetwork, stylesheets
 
 import networkx as nx
 import pandas as pd
 
-with open('./data/kg_fondecyt_uai.json', 'r') as in_file:
-    network_data = json.load(in_file)
+from data_process import filter_graph_nx_to_pyvis
+
+df = pd.read_csv('./data/proyectos_fondecyt_2012-2019.csv')
+
+# with open('./data/kg_fondecyt_uai.json', 'r') as in_file:
+#     network_data = json.load(in_file)
     
 
-network = DashNetwork(
-    id='g_fondecyt',
-    style={
-        'height': network_data['height'],
-        'width': network_data['width']
-    },
-    data={
-            'nodes': literal_eval(network_data['nodes']),
-            'edges': literal_eval(network_data['edges'])
-    }
-)
+# network = DashNetwork(
+#     id='g_fondecyt',
+#     style={
+#         'height': network_data['height'],
+#         'width': network_data['width']
+#     },
+#     data={
+#             'nodes': literal_eval(network_data['nodes']),
+#             'edges': literal_eval(network_data['edges'])
+#     }
+# )
 
 app = Dash(
     __name__,
@@ -50,13 +54,74 @@ app.layout = dbc.Container(
         ),
         dbc.Row(
             [
-                html.H2("Grafo"),
-                network
-                
+                dbc.Col(
+                    [
+                        html.H2("Grafo"),
+                        dbc.Container(
+                            # network,
+                            id='network-1'
+                        )
+                    ],
+                    width=12,
+                    class_name='mb-1'
+                )
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dbc.Container(
+                    [
+                        dcc.Dropdown(
+                            options=df['institucion_patrocinante'].unique().tolist(),
+                            value='Univ. Adolfo Ibanez',
+                            multi=False,
+                            clearable=False,
+                            searchable=True,
+                            id='dropdown-1'
+                        )
+                    ]
+                ),
+                    width={
+                        'size': 4,
+                        'offset': 1
+                    }
+                ),
+                dbc.Col(
+                    dcc.Slider(
+                        min=int(df['año_concurso'].min()),
+                        max=int(df['año_concurso'].max()),
+                        step=None,
+                        value=2019,
+                        marks={int(year): str(year) for year in df['año_concurso'].unique()},
+                        id='slider-1'
+                    )
+                )
             ]
         )
     ]
 )
+
+@app.callback(
+    Output('network-1', 'children'),
+    Input('dropdown-1', 'value'),
+    Input('slider-1', 'value')
+)
+def create_network(u_name, year):
+    G_data = filter_graph_nx_to_pyvis(df=df, year=year, university=u_name, k_layout=0.4)
+    network = DashNetwork(
+        id='g_fondecyt',
+        style={
+            'height': G_data['height'],
+            'width': G_data['width']
+        },
+        data={
+            'nodes': literal_eval(G_data['nodes']),
+            'edges': literal_eval(G_data['edges'])
+        }
+    )
+    return network
+
 
 if __name__ == "__main__":
     app.run_server(debug=True, port="9000")
---
+++ data_process.py
@@ -0,0 +1,86 @@
+import json
+
+import networkx as nx
+import numpy as np
+import pandas as pd
+from pyvis.network import Network
+
+
+
+
+def filter_graph_nx_to_pyvis(df:pd.DataFrame,
+                    year:int=None,
+                    university:str=None,
+                    k_layout:float=None) -> Network:
+    df = df.copy()
+    G = nx.MultiDiGraph()
+    if not year or not university:
+        raise TypeError("Both, year and university name, are required.")
+
+    if (year and university):
+        query = f"año_concurso == {year} & institucion_patrocinante == '{university}'"
+        df = df.query(query)
+        print(df.shape)
+    # elif year:
+    #     query = f"año_concurso == {year}"
+    #     df = df.query(query)
+    # elif university:
+    #     query = f"institucion_patrocinante == '{university}'"
+    #     df = df.query(query)
+    
+    for folio, group in df.groupby('folioproy'):
+        project_type = group['instrumento'].unique()[0]
+        researches = group['nombre_completo'].unique().tolist()
+        inst = group['institucion_patrocinante'].unique().tolist()
+        year = group['año_concurso'].unique()[0]
+        area = group['area_estudio'].unique()[0]
+        calification = group['nota_proyecto'].unique()[0]
+        # Agregado de nodos y aristas
+        G.add_node(
+            folio,
+            label=f'{folio}',
+            group='Proyectos',
+            title=f'Nota: {str(calification)}<br>Tipo: {str(project_type)}'
+        )
+        G.add_node(str(year), label=f'{year}', group='Año', title=f'Año proyectos {str(year)}')
+        G.add_node(area, label=f'{area}', group='Areas de estudio')
+        for u in inst:
+            G.add_node(u, label=f'{u}', group='Instituciones')
+            G.add_edge(folio, u, label='patrocinado_por')
+       
+        G.add_edge(folio, str(year), label='año_adjudicacion')
+        G.add_edge(folio, area, label='area_estudio', title=f'Área de estudio: {area}')
+        
+        for researcher in researches:
+            G.add_node(researcher, label=f'{researcher}', group='Investigadores')
+            G.add_edge(researcher, folio, label='investigador_de')
+    
+    if not k_layout:
+        k_layout = np.divide(1, np.sqrt(G.number_of_nodes()))
+    
+    pos = nx.spring_layout(G, k=k_layout, iterations=70)
+    xs = {k: v[0] for k, v in dict(pos).items()}
+    ys = {k: v[1] for k, v in dict(pos).items()}
+    nx.set_node_attributes(G, xs, 'x')
+    nx.set_node_attributes(G, ys, 'y')
+    nx.set_node_attributes(G, 20, 'size')
+    
+    G_pyvis = Network(
+        height="800px",
+        width="100%",
+        directed=True,
+        bgcolor="#222222",
+        font_color="#2c3e50"
+    )
+    G_pyvis.from_nx(G)
+    G_pyvis.show_buttons(filter_=['physics', "layout"])
+    G_pyvis.barnes_hut(
+        gravity=-4_000,
+        central_gravity=0,
+        spring_length=500,
+        spring_strength=0.009,
+        damping=0.025,
+        overlap=0.6
+    )
+    G_data = json.loads(G_pyvis.to_json())
+    return G_data
